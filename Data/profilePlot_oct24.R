e34d5d458aed70d5d0fad8dfa8ade2642a27d38d
richJJackson <165043314+richJJackson@users.noreply.github.com>
2024-10-16 13:36:43 +0100

    updated analysis

--- Data/profilePlot_oct24.R
+++ Data/profilePlot_oct24.R
@@ -10,19 +10,13 @@ setwd("/Users/richardjackson/Documents/github/UKOSS_CA/")
 setwd("Data")
 load("ukossData_dates.R")
 update <- read.csv("dataUpdate.csv")[1:368,]
+update2 <- read.csv("update2.csv")
 
-
-
-data2[1:3,]
-update
-
-update[1:3,]
 
 data2$GestAtAnyTypeofBirthBaby1[which(data2$TOPyes==1)]
 
 which(data2$GestAtAnyTypeofBirthBaby1>200)
 
-data2[1:3,]
 data2 <- data2[order(data2$BestguessEPPROMdate),]
 
 data2[1:3,]
@@ -59,15 +53,22 @@ antitm <- (antiDt-ppromDt)/7 + start
 
 update <- read.csv("dataUpdate.csv")[1:368,]
 
-update <- update[order(update$Gestation.at.PPROM),]
+
 update <- update[-which(update$Gestation.at.PPROM<0),]
+update <- merge(update,update2)
+update <- update[order(update$Gestation.at.PPROM),]
+
+
+update[1:3,]
 
 start <- update$Gestation.at.PPROM
 septm <- pmax(0,as.Date(update$M_SEPSIS_DATE_E1_C1,"%d/%m/%Y")-as.Date(update$Date.of.PPROM,"%d/%m/%Y"));
+septm2 <- pmax(0,as.Date(update$bs_s_dt,"%d/%m/%Y")-as.Date(update$Date.of.PPROM,"%d/%m/%Y"));
 septm <- septm+start
+septm2 <- septm2+start
 antitm <- update$Gest.at.prophylactic.antibiotics
 cstm <- update$Gest.at.steroids
-inf1tm <- as.numeric(update$Gestation.at.birth)
+inf1tm <- as.numeric(update$gest2)
 lb.cl <- as.character(factor(update$Simplified.outcome,labels=c("green","darkorchid","royalblue","black","gray")))
 
 table(lb.cl,update$Simplified.outcome)
@@ -75,9 +76,10 @@ table(lb.cl,update$Simplified.outcome)
 par(mfrow=c(1,1))
 anti.id <- which(update$ProphylacticAntibiotics==1)
 plot(0,0,xlim=c(15,42),ylim=c(0,368),bty="l",yaxt="n",xlab="Time (Weeks)",ylab="Patient ID")
-segments(start[anti.id],anti.id,rep(40,368),anti.id,lty=3,lwd=0.5,col=top.cl)
-segments(start[anti.id],anti.id,inf1tm[anti.id],anti.id,lwd=2,col=lb.cl[anti.id])
-points(septm[anti.id],anti.id,pch=20,cex=2,font=2,col=2)
+segments(start[anti.id],anti.id,rep(40,368),anti.id,lty=3,lwd=0.5,col="gray")
+segments(start[anti.id],anti.id,inf1tm[anti.id],anti.id,lwd=4,col=lb.cl[anti.id])
+#points(septm[anti.id],anti.id,pch=20,cex=2,font=2,col=2)
+points(septm2[anti.id],anti.id,pch=20,cex=1.5,font=2,col=6)
 points(cstm[anti.id],anti.id,pch="*",cex=2,font=2,col=5)
 
 legend(15,350,c("Live Birth","Spon. Loss","Still Birth","TFMR","Sepsis","Steroid"),
@@ -90,9 +92,10 @@ dev.copy2pdf(file="antiProfile_june24.pdf",height=10,width=6)
 
 anti.id <- which(is.na(update$ProphylacticAntibiotics))
 plot(0,0,xlim=c(15,42),ylim=c(0,368),bty="l",yaxt="n",xlab="Time (Weeks)",ylab="Patient ID")
-segments(start[anti.id],anti.id,rep(40,368),anti.id,lty=3,lwd=0.5,col=top.cl)
+segments(start[anti.id],anti.id,rep(40,368),anti.id,lty=3,lwd=0.5,col="gray")
 segments(start[anti.id],anti.id,inf1tm[anti.id],anti.id,lwd=2,col=lb.cl[anti.id])
-points(septm[anti.id],anti.id,pch=20,cex=2,font=2,col=2)
+#points(septm[anti.id],anti.id,pch=20,cex=2,font=2,col=2)
+points(septm2[anti.id],anti.id,pch=20,cex=1.5,font=2,col=6)
 points(cstm[anti.id],anti.id,pch="*",cex=2,font=2,col=5)
 
 legend(15,350,c("Live Birth","Spon. Loss","Still Birth","TFMR","Sepsis","Steroid"),
@@ -105,13 +108,10 @@ dev.copy2pdf(file="no_antiProfile_june24.pdf",height=10,width=6)
 ####
 ##########
 
+library(cmprsk)
 update$status <- factor(update$Simplified.outcome,labels=c("Livebirth","Spont. loss/TFMR","Stillbirth","Spont. loss/TFMR",NA))
 
-status
-
-update$Simplified.outcome
-
-table(update$Simplified.outcome)
+table(update$Simplified.outcome,update$status)
 
 antib <- as.numeric(!is.na(update$ProphylacticAntibiotics))
 
@@ -126,12 +126,8 @@ dev.copy2pdf(file="Cumulative Incidence.pdf")
 ### Table 1
 ##########
 
-
 dat.id <- which(data2$id%in%update$ParticipantID)
 anti.id <- unique(update$ParticipantID[update$ProphylacticAntibiotics==1])
-
-
-data2 <- data2[dat.id,]
 
 
 cov1 <- data2[,which(names(data2)%in%c("Gestation.at.PPROM","MULT_PREG_E1_C1",
@@ -141,56 +137,73 @@ by1 <- as.numeric(data2$id%in%anti.id)
 
 cov1$MULT_PREG_E1_C1 <- as.character(cov1$MULT_PREG_E1_C1)
 
-stab1 <- summaryTableSC(cov1,by1)
+stab1 <- summaryTable(cov1,by1)
 
 by2 <- update$ProphylacticAntibiotics
 
 by2[which(is.na(by2))] <- 0
-cov2 <- update[,which(names(update)%in%c("Gestation.at.PPROM","Gestation.at.birth","Gestation.at.Steroids","Gest.at.prophylactic.antibiotic","Simplified.outcome"))]
+cov2 <- update[,which(names(update)%in%c("Gestation.at.PPROM","gest2","Gestation.at.Steroids","Gest.at.prophylactic.antibiotic","Simplified.outcome","bs_s","Maternal.sepsis.yes.no"))]
   
 
 cov2$Gestation.at.PPROM <- as.numeric(as.character(cov2$Gestation.at.PPROM))
-cov2$Gestation.at.birth <- as.numeric(as.character(cov2$Gestation.at.birth))
+cov2$gest2 <- as.numeric(as.character(cov2$gest2))
 
-stab2 <- summaryTableSC(cov2,by2)
+stab2 <- summaryTable(cov2,by2)
+
+stab2
 
 
 write.csv(stab1,"stab1.csv")
 write.csv(stab2,"stab2.csv")
 
-getwd()
 
 
 
 cov1$eth <-factor(cov1$eth,labels=c(1,2,2,2,2,2,2,2,2,2,2,2,2,2,2))
-
 cov1$smoke2 <- factor(cov1$smoke,labels=c(1,2,3,3,3))
 
 ### Propensity score analysis
-null.mod <- glm(by1~APHpriortoEPPROM+AnyCerclage+eth+smoke2,data=cov1,family="binomial")
+## Grouping together 'rare' ethnicities
+anal.data$eth <- factor(anal.data$eth,labels=c(1,6,2,6,6,6,3,44,6,6,6,5,6,6,6))
+
+
+anal.data$eth[which(is.na(anal.data$eth))] <- 6
+anal.data$smoke2[which(is.na(anal.data$smoke2))] <- 1
+
+null.mod <- glm(by1~eth+smoke2+Gestation.at.PPROM,data=anal.data,family="binomial")
 summary(null.mod)
 anova(null.mod,test="Chisq")
 
 
-cov1[1:3,]
+### adding ps score to database
+library(boot)
+
+anal.data$ps <- inv.logit(fitted(null.mod))
+
+par(mfrow=c(1,2))
+hist(anal.data$ps,col=5,border=4,xlab="Prop. score")
+boxplot(anal.data$ps~by1,col=c(4,5),names=c("no anti.","anti"))
 
 
-summaryTableSC(cov,by)             
-             
-ProphAntiWithin2daysEPPROMDiag
-Gest.at.prophylactic.antibiotics
-Maternal.sepsis.yes.no
+
+## estimating weight
+anal.data$w <- anal.data$ps*by1 + (1-anal.data$ps)*(1-by1)
+
+
+anal.data$antib <- by1
+
+bs_mod <- glm(bs_s~antib,data=anal.data,family="binomial",weights = w)
+summLogistic(bs_mod)
+
+bs_mod <- glm(bs_s~antib,data=anal.data,family="binomial",weights = w)
+summLogistic(bs_mod)
 
 
 
-anti.id <- which(data2$exposure=="No")
-plot(0,0,xlim=c(15,42),ylim=c(0,368),bty="l",yaxt="n",xlab="Time (Weeks)",ylab="Patient ID")
-segments(start[anti.id],anti.id,rep(40,368),anti.id,lty=3,lwd=0.5,col=top.cl)
-segments(start[anti.id],anti.id,distm[anti.id],anti.id,col="orange",lwd=3)
-segments(start[anti.id],anti.id,inf1tm[anti.id],anti.id,lwd=2,col=lb.cl[anti.id])
-points(septm[anti.id],anti.id,pch=20,cex=2,font=2,col=2)
-points(cstm[anti.id],anti.id,pch="*",cex=2,font=2,col=5)
-dev.copy2pdf(file="no_antiProfile_june24.pdf",height=10,width=6)
+### Adding weights to data
+
+
+### assessing outcomes
 
 
 
