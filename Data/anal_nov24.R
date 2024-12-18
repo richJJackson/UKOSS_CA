d984805fbf9f0f82a90a1f71e763db305b6fa1a6
richJJackson <165043314+richJJackson@users.noreply.github.com>
2024-12-18 09:05:55 +0000

    update

--- Data/anal_nov24.R
+++ Data/anal_nov24.R
@@ -120,19 +120,22 @@ names(data67)[14] <- "pa"
 data125$pa <- factor(data125$pa,labels=c("None","Eryth.","IV","Alternative"))
 data125$pa2 <- factor(data125$pa,labels=c("None","Anti","Anti","Anti"))
 data125$pa3 <- factor(data125$pa,labels=c(0,1,1,1))
+data125$pa4 <- factor(data125$pa,labels=c(0,1,NA,NA))
 
 data3$pa <- factor(data3$pa,labels=c("None","Eryth.","IV","Alternative"))
 data3$pa2 <- factor(data3$pa,labels=c("None","Anti","Anti","Anti"))
 data3$pa3 <- factor(data3$pa,labels=c(0,1,1,1))
-
+data3$pa4 <- factor(data3$pa,labels=c(0,1,NA,NA))
 
 data4$pa <- factor(data4$pa,labels=c("None","Eryth.","IV","Alternative"))
 data4$pa2 <- factor(data4$pa,labels=c("None","Anti","Anti","Anti"))
 data4$pa3 <- factor(data4$pa,labels=c(0,1,1,1))
+data4$pa4 <- factor(data4$pa,labels=c(0,1,NA,NA))
 
 data67$pa <- factor(data67$pa,labels=c("None","Eryth.","IV","Alternative"))
 data67$pa2 <- factor(data67$pa,labels=c("None","Anti","Anti","Anti"))
 data67$pa3 <- factor(data67$pa,labels=c(0,1,1,1))
+data67$pa4 <- factor(data67$pa,labels=c(0,1,NA,NA))
 
 
 
@@ -175,6 +178,9 @@ prop.cov.67$simpleEthnicity[which(prop.cov.67$simpleEthnicity=="")] <- "Other"
 prop.cov.67$simpleEthnicity <- factor(prop.cov.67$simpleEthnicity,labels=c("Other","Other","Other","Other","White"))
 
 
+
+
+
 ####################################################################
 ####################################################################
 ######### Question 1
@@ -186,13 +192,15 @@ step.mod <- step(ps.mod,direction="backward",k=6)
 summary(step.mod)
 
 ps <- fitted(step.mod)
-mod.data <- data.frame("out"=data125$Woman.sick,"pa3"=data125$pa3,"ps"=ps)
+mod.data <- data.frame("out"=data125$Woman.sick,"pa3"=data125$pa3,"pa4"=data125$pa4,"ps"=ps)
 
 mod.data$ps2 <- 1- mod.data$ps
 mod.data$w <- mod.data$ps*(as.numeric(mod.data$pa3)-1) + mod.data$ps2*(2-as.numeric(mod.data$pa3))
 
 null.mod <- glm(out~pa3,data=mod.data,family="binomial")
 psa.mod <- glm(out~pa3,data=mod.data,family="binomial",weight=1/w)
+sen.mod.1 <- glm(out~pa4,data=mod.data,family="binomial",weight=1/w)
+
 
 ### plot results
 tb1 <- percTable(data125$Woman.sick,data125$pa2,row=F)
@@ -215,6 +223,8 @@ colnames(mod.res) <-c("c1","c2","c3")
 
 res_1 <- rbind(nmt,tb.res,"",nmm,mod.res)
 
+res_1
+
 
 
 ####################################################################
@@ -223,16 +233,16 @@ res_1 <- rbind(nmt,tb.res,"",nmm,mod.res)
 
 data125[1:3,]
 
-mod.data <- data.frame("out"=data125$Liverbirth.or.not..1.livebirth.0.not.,"pa3"=data125$pa3,"ps"=ps)
+mod.data <- data.frame("out"=data125$Liverbirth.or.not..1.livebirth.0.not.,"pa3"=data125$pa3,"pa4"=data125$pa4,"ps"=ps)
 mod.data$ps2 <- 1- mod.data$ps
 mod.data$w <- mod.data$ps*(as.numeric(mod.data$pa3)-1) + mod.data$ps2*(2-as.numeric(mod.data$pa3))
-mod.data[1:3,]
 
 null.mod <- glm(out~pa3,data=mod.data,family="binomial")
 psa.mod <- glm(out~pa3,data=mod.data,family="binomial",weight=1/w)
+sen.mod.2 <- glm(out~pa4,data=mod.data,family="binomial",weight=1/w)
 
 ### plot results
-tb1 <- percTable(data125$Woman.sick,data125$pa2,row=F)
+tb1 <- percTable(data125$Liverbirth.or.not..1.livebirth.0.not.,data125$pa2,row=F)
 tb2.1 <- tapply(mod.data$ps,mod.data$pa3,summ.mean,se=F)
 tb2.2 <- summ.mean(mod.data$ps)
 
@@ -252,6 +262,9 @@ colnames(mod.res) <-c("c1","c2","c3")
 
 res_2 <- rbind(nmt,tb.res,"",nmm,mod.res)
 
+
+res_2
+
 ####################################################################
 ####################################################################
 ######### Question 3
@@ -261,13 +274,14 @@ ps.mod <- glm(data3$pa3~.,data=prop.cov.3,family="binomial")
 step.mod <- step(ps.mod,direction="backward",k=6)
 
 ps <- fitted(step.mod)
-mod.data <- data.frame("out"=data3$Woman.sick,"pa3"=data3$pa3,"ps"=ps)
+mod.data <- data.frame("out"=data3$Woman.sick,"pa3"=data3$pa3,"pa4"=data3$pa4,"ps"=ps)
 
 mod.data$ps2 <- 1- mod.data$ps
 mod.data$w <- mod.data$ps*(as.numeric(mod.data$pa3)-1) + mod.data$ps2*(2-as.numeric(mod.data$pa3))
 
 null.mod <- glm(out~pa3,data=mod.data,family="binomial")
 psa.mod <- glm(out~pa3,data=mod.data,family="binomial",weight=1/w)
+sen.mod.3 <- glm(out~pa4,data=mod.data,family="binomial",weight=1/w)
 
 ### plot results
 tb1 <- percTable(data3$Woman.sick,data3$pa2,row=F)
@@ -302,13 +316,14 @@ ps.mod <- glm(data4$pa3~.,data=prop.cov.4,family="binomial")
 step.mod <- step(ps.mod,direction="backward",k=6)
 
 ps <- fitted(step.mod)
-mod.data <- data.frame("out"=data4$Liverbirth.or.not..1.livebirth.0.not.,"pa3"=data4$pa3,"ps"=ps)
+mod.data <- data.frame("out"=data4$Liverbirth.or.not..1.livebirth.0.not.,"pa3"=data4$pa3,"pa4"=data4$pa4,"ps"=ps)
 
 mod.data$ps2 <- 1- mod.data$ps
 mod.data$w <- mod.data$ps*(as.numeric(mod.data$pa3)-1) + mod.data$ps2*(2-as.numeric(mod.data$pa3))
 
 null.mod <- glm(out~pa3,data=mod.data,family="binomial")
 psa.mod <- glm(out~pa3,data=mod.data,family="binomial",weight=1/w)
+sen.mod.4 <- glm(out~pa4,data=mod.data,family="binomial",weight=1/w)
 
 ### plot results
 tb1 <- percTable(data4$Liverbirth.or.not..1.livebirth.0.not.,data4$pa2,row=F)
@@ -341,14 +356,14 @@ data4[1:3,1:10]
 
 data4$out2 <- data4$Woman.sick==0&data4$Liverbirth.or.not..1.livebirth.0.not.==1
 
-
-mod.data <- data.frame("out"=data4$out2,"pa3"=data4$pa3,"ps"=ps)
+mod.data <- data.frame("out"=data4$out2,"pa3"=data4$pa3,"pa4"=data4$pa4,"ps"=ps)
 
 mod.data$ps2 <- 1- mod.data$ps
 mod.data$w <- mod.data$ps*(as.numeric(mod.data$pa3)-1) + mod.data$ps2*(2-as.numeric(mod.data$pa3))
 
 null.mod <- glm(out~pa3,data=mod.data,family="binomial")
 psa.mod <- glm(out~pa3,data=mod.data,family="binomial",weight=1/w)
+sen.mod.4.2 <- glm(out~pa4,data=mod.data,family="binomial",weight=1/w)
 
 ### plot results
 tb1 <- percTable(data4$out2,data4$pa2,row=F)
@@ -378,13 +393,22 @@ res_4.2
 ######### Question 5
 
 
+### propensity socre
+ps.mod <- glm(data125$pa3~.,data=prop.cov,family="binomial")
 
-mod.data <- data.frame("out"=data125$Whole.pregnancy.outcome..mum.sepsis.free.and.livebirth.,"pa3"=data125$pa3,"ps"=ps)
+step.mod <- step(ps.mod,direction="backward",k=6)
+summary(step.mod)
+
+ps <- fitted(step.mod)
+
+mod.data <- data.frame("out"=data125$Whole.pregnancy.outcome..mum.sepsis.free.and.livebirth.,
+                       "pa3"=data125$pa3,"pa4"=data125$pa4,"ps"=ps)
 mod.data$ps2 <- 1- mod.data$ps
 mod.data$w <- mod.data$ps*(as.numeric(mod.data$pa3)-1) + mod.data$ps2*(2-as.numeric(mod.data$pa3))
 
 null.mod <- glm(out~pa3,data=mod.data,family="binomial")
 psa.mod <- glm(out~pa3,data=mod.data,family="binomial",weight=1/w)
+sen.mod.5 <- glm(out~pa4,data=mod.data,family="binomial",weight=1/w)
 
 ### plot results
 tb1 <- percTable(data125$Woman.sick,data125$pa2,row=F)
@@ -422,13 +446,14 @@ step.mod <- step(ps.mod,direction="backward",k=6)
 data67[1:3,1:10]
 
 ps <- fitted(step.mod)
-mod.data <- data.frame("out"=data67$Mum.well.and.baby.survive.to.discharge,"pa3"=data67$pa3,"ps"=ps)
+mod.data <- data.frame("out"=data67$Mum.well.and.baby.survive.to.discharge,"pa3"=data67$pa3,"pa4"=data67$pa4,"ps"=ps)
 
 mod.data$ps2 <- 1- mod.data$ps
 mod.data$w <- mod.data$ps*(as.numeric(mod.data$pa3)-1) + mod.data$ps2*(2-as.numeric(mod.data$pa3))
 
 null.mod <- glm(out~pa3,data=mod.data,family="binomial")
 psa.mod <- glm(out~pa3,data=mod.data,family="binomial",weight=1/w)
+sen.mod.6 <- glm(out~pa4,data=mod.data,family="binomial",weight=1/w)
 
 ### plot results
 tb1 <- percTable(data67$Mum.well.and.baby.survive.to.discharge,data67$pa2,row=F)
@@ -457,12 +482,16 @@ res_6
 ####################################################################
 ######### Question 7
 
-mod.data <- data.frame("out"=data67$Mum.well.and.baby.without.major.morbidity.at.discharge,"pa3"=data67$pa3,"ps"=ps)
+mod.data <- data.frame("out"=data67$Mum.well.and.baby.without.major.morbidity.at.discharge,
+                       "pa3"=data67$pa3,"pa4"=data67$pa4,"ps"=ps)
+
 mod.data$ps2 <- 1- mod.data$ps
 mod.data$w <- mod.data$ps*(as.numeric(mod.data$pa3)-1) + mod.data$ps2*(2-as.numeric(mod.data$pa3))
 
 null.mod <- glm(out~pa3,data=mod.data,family="binomial")
 psa.mod <- glm(out~pa3,data=mod.data,family="binomial",weight=1/w)
+sen.mod.7 <- glm(out~pa4,data=mod.data,family="binomial",weight=1/w)
+
 
 ### plot results
 tb1 <- percTable(data67$Mum.well.and.baby.without.major.morbidity.at.discharge,data67$pa2,row=F)
@@ -487,6 +516,15 @@ res_7 <- rbind(nmt,tb.res,"",nmm,mod.res)
 
 
 
-library(knitr)
-kable(res_4.2)
 
+######## sensitivity analysis (erythromycin only)
+
+sen.res <- rbind(summLogistic(sen.mod.1)[2,],summLogistic(sen.mod.2)[2,],
+                 summLogistic(sen.mod.3)[2,],summLogistic(sen.mod.4)[2,],
+                 summLogistic(sen.mod.4.2)[2,],summLogistic(sen.mod.5)[2,],
+                 summLogistic(sen.mod.6)[2,],summLogistic(sen.mod.7)[2,])
+
+kable(sen.res)
+
+
+############ pro
--- Data/profilePlot_oct24.R
+++ Data/profilePlot_oct24.R
@@ -12,6 +12,8 @@ load("ukossData_dates.R")
 update <- read.csv("dataUpdate.csv")[1:368,]
 update2 <- read.csv("update2.csv")
 
+data2[1:3,]
+
 
 data2$GestAtAnyTypeofBirthBaby1[which(data2$TOPyes==1)]
 
@@ -52,55 +54,111 @@ antitm <- (antiDt-ppromDt)/7 + start
 ###############################
 
 update <- read.csv("dataUpdate.csv")[1:368,]
-
-
-update <- update[-which(update$Gestation.at.PPROM<0),]
 update <- merge(update,update2)
+
+### multiple preg
+update$mult.id <- as.numeric(update$MULT_PREG_E1_C1..also.exlcude.at.the.moment.==1)
+update$postNat <- as.numeric(update$ParticipantID=="EPR_377")
+update$priorSep <- as.numeric(update$ParticipantID%in%c("EPR_242","EPR_452","EPR_180"))
+update$unkOut <- as.numeric(update$Simplified.outcome=="Unknown outcome")
+
+
+
+### 6 patients with unknown pprom date - removed
+update <- update[-which(update$Gestation.at.PPROM<0),]
+
+
+### ordering gestation
 update <- update[order(update$Gestation.at.PPROM),]
 
 
-update[1:3,]
+### multiple preg
+multDat <- update[which(update$mult.id==1),]
+save(multDat,file="multDat.R")
 
-start <- update$Gestation.at.PPROM
-septm <- pmax(0,as.Date(update$M_SEPSIS_DATE_E1_C1,"%d/%m/%Y")-as.Date(update$Date.of.PPROM,"%d/%m/%Y"));
-septm2 <- pmax(0,as.Date(update$bs_s_dt,"%d/%m/%Y")-as.Date(update$Date.of.PPROM,"%d/%m/%Y"));
-septm <- septm+start
-septm2 <- septm2+start
-antitm <- update$Gest.at.prophylactic.antibiotics
-cstm <- update$Gest.at.steroids
-inf1tm <- as.numeric(update$gest2)
-lb.cl <- as.character(factor(update$Simplified.outcome,labels=c("green","darkorchid","royalblue","black","gray")))
 
-table(lb.cl,update$Simplified.outcome)
+### filtering dataset
+update <- update[-which(update$mult.id==1|update$unkOut==1),]
+
+### id for patients with a sepsis date of 0 or 1 days
+septm <- pmax(0,as.Date(update$M_SEPSIS_DATE_E1_C1,"%d/%m/%Y")-as.Date(update$Date.of.PPROM,"%d/%m/%Y"));septm
+update$postSep <- as.numeric(septm<2);update$postSep
+
+
+
+which(update$priorSep==1|update$postSep==1)
+
+
+
+update$M_SEPSIS_DATE_E1_C1
+
+
+update$start <- update$Gestation.at.PPROM
+update$septm <- pmax(0,as.Date(update$M_SEPSIS_DATE_E1_C1,"%d/%m/%Y")-as.Date(update$Date.of.PPROM,"%d/%m/%Y"));
+update$septm2 <- pmax(0,as.Date(update$bs_s_dt,"%d/%m/%Y")-as.Date(update$Date.of.PPROM,"%d/%m/%Y"));
+update$septm <- update$septm+update$start
+update$septm2 <- update$septm2+update$start
+update$antitm <- update$Gest.at.prophylactic.antibiotics
+update$cstm <- update$Gest.at.steroids
+update$inf1tm <- as.numeric(update$gest2)
+update$lb.cl <- as.character(factor(update$Simplified.outcome,labels=c("forestgreen","darkorchid","royalblue","navyblue")))
+
+
+### ordering gestation
+update <- update[order(update$Gestation.at.PPROM),];update$septm
+#update <- update[order(update$priorSep),];update$septm
+#update <- update[order(update$postSep),];update$septm
+
+
+anti.id <- which(update$ProphylacticAntibiotics==1&update$priorSep!=1&(is.na(update$postSep)|update$postSep==0))
+d <- update[anti.id,]
 
 par(mfrow=c(1,1))
-anti.id <- which(update$ProphylacticAntibiotics==1)
-plot(0,0,xlim=c(15,42),ylim=c(0,368),bty="l",yaxt="n",xlab="Time (Weeks)",ylab="Patient ID")
-segments(start[anti.id],anti.id,rep(40,368),anti.id,lty=3,lwd=0.5,col="gray")
-segments(start[anti.id],anti.id,inf1tm[anti.id],anti.id,lwd=4,col=lb.cl[anti.id])
+plot(0,0,xlim=c(15,42),ylim=c(0,nrow(d)),bty="l",yaxt="n",xlab="Time (Weeks)",ylab="Patient ID")
+segments(d$start,1:nrow(d),rep(40,nrow(d)),1:nrow(d),lty=3,lwd=0.5,col="gray")
+segments(d$start,1:nrow(d),d$inf1tm,1:nrow(d),lwd=4,col=d$lb.cl)
 #points(septm[anti.id],anti.id,pch=20,cex=2,font=2,col=2)
-points(septm2[anti.id],anti.id,pch=20,cex=1.5,font=2,col=6)
-points(cstm[anti.id],anti.id,pch="*",cex=2,font=2,col=5)
+points(d$septm,1:nrow(d),pch=20,cex=1.5,font=2,col=6)
+points(d$cstm,1:nrow(d),pch="*",cex=2,font=2,col=5)
 
-legend(15,350,c("Live Birth","Spon. Loss","Still Birth","TFMR","Sepsis","Steroid"),
-       col=c("green","darkorchid","royalblue","black",2,5),pch=c(rep("-",4),"o","*"),bty="n")
+legend(15,180,c("Live Birth","Spon. Loss","Still Birth","TFMR","Sepsis","Steroid"),
+       col=c("green","darkorchid","royalblue","navyblue",2,5),pch=c(rep("-",4),"o","*"),bty="n",lwd=3)
 dev.copy2pdf(file="antiProfile_june24.pdf",height=10,width=6)
 
 
 
 
+no.anti.id <- which(is.na(update$ProphylacticAntibiotics)&update$priorSep!=1&(is.na(update$postSep)|update$postSep==0))
+d <- update[no.anti.id,]
 
-anti.id <- which(is.na(update$ProphylacticAntibiotics))
-plot(0,0,xlim=c(15,42),ylim=c(0,368),bty="l",yaxt="n",xlab="Time (Weeks)",ylab="Patient ID")
-segments(start[anti.id],anti.id,rep(40,368),anti.id,lty=3,lwd=0.5,col="gray")
-segments(start[anti.id],anti.id,inf1tm[anti.id],anti.id,lwd=2,col=lb.cl[anti.id])
+par(mfrow=c(1,1))
+plot(0,0,xlim=c(15,42),ylim=c(0,nrow(d)),bty="l",yaxt="n",xlab="Time (Weeks)",ylab="Patient ID")
+segments(d$start,1:nrow(d),rep(40,nrow(d)),1:nrow(d),lty=3,lwd=0.5,col="gray")
+segments(d$start,1:nrow(d),d$inf1tm,1:nrow(d),lwd=4,col=d$lb.cl)
 #points(septm[anti.id],anti.id,pch=20,cex=2,font=2,col=2)
-points(septm2[anti.id],anti.id,pch=20,cex=1.5,font=2,col=6)
-points(cstm[anti.id],anti.id,pch="*",cex=2,font=2,col=5)
+points(d$septm,1:nrow(d),pch=20,cex=1.5,font=2,col=6)
+points(d$cstm,1:nrow(d),pch="*",cex=2,font=2,col=5)
 
-legend(15,350,c("Live Birth","Spon. Loss","Still Birth","TFMR","Sepsis","Steroid"),
-       col=c("green","darkorchid","royalblue","black",2,5),pch=c(rep("-",4),"o","*"),bty="n")
-dev.copy2pdf(file="no_antiProfile_june24.pdf",height=10,width=6)
+legend(15,100,c("Live Birth","Spon. Loss","Still Birth","TFMR","Sepsis","Steroid"),
+       col=c("green","darkorchid","royalblue","navyblue",2,5),pch=c(rep("-",4),"o","*"),bty="n",lwd=3)
+dev.copy2pdf(file="NOantiProfile_dev24.pdf",height=10,width=6)
+
+
+
+sep.id <- which(update$priorSep==1|update$postSep==1)
+d <- update[sep.id,]
+
+par(mfrow=c(1,1))
+plot(0,0,xlim=c(15,42),ylim=c(0,nrow(d)),bty="l",yaxt="n",xlab="Time (Weeks)",ylab="Patient ID")
+segments(d$start,1:nrow(d),rep(40,nrow(d)),1:nrow(d),lty=3,lwd=0.5,col="gray")
+segments(d$start,1:nrow(d),d$inf1tm,1:nrow(d),lwd=4,col=d$lb.cl)
+#points(septm[anti.id],anti.id,pch=20,cex=2,font=2,col=2)
+points(d$septm,1:nrow(d),pch=20,cex=1.5,font=2,col=6)
+points(d$cstm,1:nrow(d),pch="*",cex=2,font=2,col=5)
+
+legend(15,100,c("Live Birth","Spon. Loss","Still Birth","TFMR","Sepsis","Steroid"),
+       col=c("green","darkorchid","royalblue","navyblue",2,5),pch=c(rep("-",4),"o","*"),bty="n",lwd=3)
+dev.copy2pdf(file="NOantiProfile_dev24.pdf",height=10,width=6)
 
 
 
